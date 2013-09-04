2d9a0cf045904d0613b22b0cd1c84f18d8a6769e
Hack Reactor Students <github@hackreactor.com>
2013-09-04 15:59:36 -0700

    Added barebones buttons for voting

--- js/controllers.js
+++ js/controllers.js
@@ -2,19 +2,42 @@ angular.module('myApp.controllers', [])
 .controller('MainController', 
   ['$scope','$http',
   function($scope,$http) {
+    // including mine for now, until we get logins to work.
+    var modhash = 
 
-  $http({
-      method: 'JSONP',
-      url: 'http://www.reddit.com/hot.json?jsonp=JSON_CALLBACK'
-    }).success(function(data, status, headers, config) {
-      // data contains the response
-      // status is the HTTP status
-      // headers is the header getter function
-      // config is the object that was used to create the HTTP request
-      console.log('success',data.data.children);
-      $scope.articles = data.data.children;
-    }).error(function(data, status, headers, config) {
-      console.log('error',data);
-    });
+    $http({
+        method: 'JSONP',
+        url: 'http://www.reddit.com/hot.json?jsonp=JSON_CALLBACK'
+      }).success(function(data, status, headers, config) {
+        // data contains the response
+        // status is the HTTP status
+        // headers is the header getter function
+        // config is the object that was used to create the HTTP request
+        console.log('success',data.data.children);
+        $scope.articles = data.data.children;
+      }).error(function(data, status, headers, config) {
+        console.log('error',data);
+      });
+
+    $scope.upvoteArticle = function(articleId) {
+      $http({
+        method: 'POST',
+        url: 'http://www.reddit.com/api/vote',
+        data: {
+          dir: 1,
+          id: articleId,
+          uh: 
+        }
+      }).success(function(data, status, headers, config) {
+        // data contains the response
+        // status is the HTTP status
+        // headers is the header getter function
+        // config is the object that was used to create the HTTP request
+        console.log('success',data.data.children);
+        $scope.articles = data.data.children;
+      }).error(function(data, status, headers, config) {
+        console.log('error',data);
+      });
+    };
 
 }]);
--- js/directives.js
+++ js/directives.js
@@ -8,7 +8,10 @@ angular.module('myApp.directives', [])
     '<h4>{{ ngModel.title }}</h4>'+
     '<img ng-src="{{ ngModel.thumbnail }}" /></a><br />'+
     '{{ ngModel.score }} <br />'+
-    '{{ ngModel.ups }} up, {{ ngModel.downs }} down <br />'+
+    //'{{ ngModel.ups }} up, {{ ngModel.downs }} down <br />'+
+    '<button ng-click="upvoteArticle({{ ngModel.id}})">awesome!</button>' +
+    '<button ng-click="unvoteArticle()">meh</button>' +
+    '<button ng-click="downvoteArticle()">booo-urns!</button>' +
     '{{ ngModel.num_comments }} comments',
     //templateURL: 'article.html',
     link: function(scope, ele, attrs, ctrl) {
