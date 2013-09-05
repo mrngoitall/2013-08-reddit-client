29f5b9caebeb6bd32bba68d086e61310e98df7db
Hack Reactor Students <github@hackreactor.com>
2013-09-04 19:58:26 -0700

    Minor code cleanup

--- js/directives.js
+++ js/directives.js
@@ -1,9 +1,6 @@
 angular.module('myApp.directives', [])
 .directive('myArticle', function() {
   return { 
-    //replace: true,
-    //require: 'ngModel',
-    //template: '{{ ngModel.title }}',
     templateUrl: '/templates/newsItem.html',
     link: function(scope, ele, attrs, ctrl) {
       scope.$watch(attrs.ngModel,
