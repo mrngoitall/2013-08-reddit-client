f4c77f72e8bc64de352e2bfce9f4d767ee293f70
William Ngo <will@mrngoitall.net>
2013-09-06 23:48:26 -0700

    Merged code from Ari lecture

--- js/app.js
+++ js/app.js
@@ -6,12 +6,45 @@ angular.module('myApp', ['ngRoute', 'myApp.controllers', 'myApp.directives', 'my
   .when('/',
     {
       controller: 'MainController',
-      templateUrl: '../templates/home.html'
+      templateUrl: '../templates/home.html',
+      resolve: {
+        user: ['UserService', function(UserService) {
+          return UserService.getCurrentUser();
+        }]
+      }
     })
   .when('/login',
     {
       controller: 'LoginController',
       templateUrl: '../templates/login.html'
     })
+  .when('/settings',
+    { 
+      controller: 'SettingsController',
+      templateUrl: '../templates/settings.html'
+    })
   .otherwise({ redirectTo: '/' });
+})
+.config(function($httpProvider) {
+  var interceptor = ['$rootScope', '$location', '$q',
+  function($scope, $location, $q) {
+    var success = function(resp) {return resp; };
+    var err = function(resp, status) {
+      if (resp.status !== 200) {
+        var d = $q.defer();
+        $scope.$broadcast('event:unauthorized');
+        return d.promise;
+      }
+      return $q.reject(resp);
+    };
+    return function(promise) {
+      return promise.then(success, err);
+    };
+  }];
+  $httpProvider.responseInterceptors.push(interceptor);
+})
+.run(function($rootScope, $http, $location) {
+  $rootScope.$on('event:unauthorized', function() {
+    $location.path('/login');
+  });
 });
